e33fd29a2d82a663042988f4d380ba3a025bc94b
Serega1067 <serega1067@yandex.ru>
2024-12-02 23:47:08 +0700

    add test testInput

---
+++ src/main/java/com/project/task1/FizzBuzzSimpleGame.java
@@ -0,0 +1,9 @@
+package com.project.task1;
+
+public class FizzBuzzSimpleGame implements FizzBuzzSimple {
+    @Override
+    public String print(int number) {
+//        throw new UnsupportedOperationException("todo: implement this");
+        return "";
+    }
+}
---
+++ src/test/java/com/project/task1/FizzBuzzSimpleGameTest.java
@@ -0,0 +1,13 @@
+package com.project.task1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzSimpleGameTest {
+
+    @Test
+    void testInput() {
+        new FizzBuzzSimpleGame().print(0);
+    }
+}
