35addb56ba1ecfb978915e3f31082e84ba353534
Serega1067 <serega1067@yandex.ru>
2024-12-04 04:24:32 +0700

    testCorrectInput, testIncorrectInput, testFizz

--- src/main/java/com/project/task1/FizzBuzzSimpleGame.java
+++ src/main/java/com/project/task1/FizzBuzzSimpleGame.java
@@ -1,9 +1,25 @@
 package com.project.task1;
 
 public class FizzBuzzSimpleGame implements FizzBuzzSimple {
+
     @Override
     public String print(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(
+                    "number should be great or equal 0"
+            );
+        }
+        if (number > 100) {
+            throw new IllegalArgumentException(
+                    "number should be less or equal 100"
+            );
+        }
 //        throw new UnsupportedOperationException("todo: implement this");
+
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
         return "";
     }
 }
--- src/test/java/com/project/task1/FizzBuzzSimpleGameTest.java
+++ src/test/java/com/project/task1/FizzBuzzSimpleGameTest.java
@@ -1,13 +1,47 @@
 package com.project.task1;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzSimpleGameTest {
 
+    private static FizzBuzzSimple fizzBuzzSimple;
+
+    @BeforeAll
+    static void createFizzBuzz() {
+        fizzBuzzSimple = new FizzBuzzSimpleGame();
+    }
+
     @Test
-    void testInput() {
-        new FizzBuzzSimpleGame().print(0);
+    void testCorrectInput() {
+        fizzBuzzSimple.print(0);
+        fizzBuzzSimple.print(100);
+    }
+
+    @Test
+    void testIncorrectInput() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    fizzBuzzSimple.print(-1);
+                }
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    fizzBuzzSimple.print(101);
+                }
+        );
+    }
+
+    @Test
+    void testFizz() {
+        assertEquals("Fizz", fizzBuzzSimple.print(3));
+        assertEquals("Fizz", fizzBuzzSimple.print(6));
+        assertEquals("Fizz", fizzBuzzSimple.print(9));
+        assertEquals("Fizz", fizzBuzzSimple.print(12));
+        assertEquals("Fizz", fizzBuzzSimple.print(15));
     }
 }
